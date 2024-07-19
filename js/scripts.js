a3a01e69d610402c051423cde497167e1d9d459c
SJuniorDuarte <133438446+SJuniorDuarte@users.noreply.github.com>
2024-07-18 21:31:43 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+const buttons = document.querySelectorAll("#image-picker li");
+const image = document.getElementById("product-image");
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    buttons.forEach((btn) => {
+      btn.querySelector(".color").classList.remove("selected");
+    });
+    const button = e.target;
+    const id = button.getAttribute("id");
+    console.log(id);
+    button.querySelector(".color").classList.add("selected");
+    image.classList.add("changing");
+    image.setAttribute("src", `img/iphone_${id}.jpg`);
+    setTimeout(() => image.classList.remove("changing"), 100);
+  });
+});
